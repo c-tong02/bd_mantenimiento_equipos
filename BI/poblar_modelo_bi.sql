919acdb3f83dd2ec2b5bff2b999024c60f4478e7
Carlos Tong <carlosatongr@gmail.com>
2025-04-07 20:34:52 -0500

    Update poblar_modelo_bi.sql

--- BI/poblar_modelo_bi.sql
+++ BI/poblar_modelo_bi.sql
@@ -50,24 +50,3 @@ GROUP BY
 
 select*
 from mantenimientos
-
-SELECT
-	e.id as 'id_dim_equipos',
-	t.id as 'id_dim_tecnicos',
-	fecha_mantenimiento as 'id_dim_fechas',
-	DATEDIFF(hour, fecha_inicio, fecha_fin) AS 'duracion_total_mantenimiento',
-	SUM(ur.cantidad_usada) AS 'cantidad_repuestos_usados',
-	SUM(r.costo_unitario * ur.cantidad_usada) AS 'costo_repuestos',
-	t.costo_hora*DATEDIFF(hour, fecha_inicio, fecha_fin) AS 'costo_operativo',
-	t.costo_hora*DATEDIFF(hour, fecha_inicio, fecha_fin) + SUM(r.costo_unitario * ur.cantidad_usada) AS 'costo_total_mantenimiento'
-FROM mantenimientos m
-	INNER JOIN equipos e ON e.id = m.id_equipo
-	INNER JOIN uso_de_repuestos ur ON m.id = ur.id_mantenimiento
-	INNER JOIN tecnicos t ON t.id = m.id_tecnico
-	INNER JOIN repuestos r ON r.id = ur.id_repuesto
-GROUP BY
-	e.id,
-	t.id,
-	fecha_mantenimiento,
-	DATEDIFF(hour, fecha_inicio, fecha_fin),
-	t.costo_hora*DATEDIFF(hour, fecha_inicio, fecha_fin);
